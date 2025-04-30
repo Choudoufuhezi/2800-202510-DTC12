4bdc6a19031c55889fef5a2139788b65953ef1f8
Derek C <dcao12@my.bcit.ca>
2025-04-29 21:16:20 -0700

    Create email helper service in order to send verification emails to users

--- backend/config.py
+++ backend/config.py
@@ -13,6 +13,12 @@ class Settings(BaseSettings):
     google_client_secret: str
     google_redirect_uri: str
     
+    # SMTP settings
+    smtp_server: str
+    smtp_port: int
+    smtp_username: str
+    smtp_password: str
+    
     class Config:
         env_file = ".env"
         env_file_encoding = 'utf-8'  # recommended
