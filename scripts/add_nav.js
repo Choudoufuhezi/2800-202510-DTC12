b1e7b51a7648feb45a545f5944f65f7055c924bc
Courtney Lum <courtney.jlum@gmail.com>
2025-05-01 01:02:38 -0700

    Refactor navigation and sidebar loading logic

--- scripts/add_nav.js
+++ scripts/add_nav.js
@@ -1,11 +1,39 @@
-fetch("components/nav_top_auth.html")
+// Load top nav and attach sidebar logic after it's added
+fetch("./components/nav_top_auth.html")
     .then(response => response.text())
     .then(html => {
-        document.getElementById("topnav").innerHTML = html
-    })
+        document.getElementById("topNav").innerHTML = html;
 
-fetch("components/nav_bottom.html")
+        const showSidebar = document.getElementById("showSidebar");
+        const sidebar = document.getElementById("sidebar");
+        const closeSidebar = document.getElementById("closeSidebar");
+        const overlay = document.getElementById("overlay");
+
+        showSidebar.addEventListener("click", () => {
+            console.log("Sidebar button clicked");
+            sidebar.classList.remove("hidden");
+            overlay.classList.remove("hidden");
+
+        });
+        closeSidebar.addEventListener("click", () => {
+            sidebar.classList.add("hidden");
+            overlay.classList.add("hidden");
+        });
+    
+    });
+
+// Load bottom nav
+fetch("./components/nav_bottom.html")
     .then(response => response.text())
     .then(html => {
-        document.getElementById("botnav").innerHTML = html
-    })
+        document.getElementById("bottomNav").innerHTML = html;
+       
+        
+            const pages = document.querySelectorAll("nav a");
+            pages.forEach(page => {
+                if (page.href === window.location.href) {
+                    page.classList.add("bg-sky-400");
+                }
+            });
+
+    });
