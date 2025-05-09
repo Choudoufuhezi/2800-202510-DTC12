9ba89d40239f1804c465aa7107ec7d661a6abf2d
Hank Zhao <hankzhao32@gmail.com>
2025-05-09 00:33:06 -0700

    Fix typo in the upload page

--- scripts/upload.js
+++ scripts/upload.js
@@ -9,7 +9,7 @@ function showSuccessPopup() {
     }
 
     // Show modal
-    const modal = document.getEleme ntById("successModal");
+    const modal = document.getElementById("successModal");
     modal.classList.remove("hidden");
 
     // Optional: Update modal content
