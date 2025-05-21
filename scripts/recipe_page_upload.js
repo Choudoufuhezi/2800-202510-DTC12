b7fb06fc29a2e2fe5ec9590965ccdd802c7abdf7
Derek C <dcao12@my.bcit.ca>
2025-05-21 14:40:37 -0700

    Create eslint config and adjust Javascript based on its recommendations

--- scripts/recipe_page_upload.js
+++ scripts/recipe_page_upload.js
@@ -439,7 +439,7 @@ async function modal(img, memory) {
         if (comments.length === 0) {
             commentsList.innerHTML = "<p class='text-gray-500 italic'>No comments yet.</p>";
         } else {
-            comments.forEach((c, index) => {
+            comments.forEach((c) => {
                 const familyMember = familyMembers.find(member => member.user_id === c.user_id);
                 const userName = familyMember ?
                     (familyMember.custom_name ? familyMember.custom_name : familyMember.email)
