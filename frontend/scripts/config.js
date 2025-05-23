20edd9dd09811466b421a06e101f12f5413f272b
Hank Zhao <hankzhao32@gmail.com>
2025-05-23 16:12:47 -0700

    Fix minor issues in the profile page

--- frontend/scripts/config.js
+++ frontend/scripts/config.js
@@ -1,4 +1,3 @@
 // scripts/config.js
 export const API_URL = "https://two800-202510-dtc12-0d55.onrender.com"; // Backend on Render
 export const BASE_URL = "https://dev--digitalfamilyvault.netlify.app"; // Frontend running locally
-
