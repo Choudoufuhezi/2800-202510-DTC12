3524822c07e55128632c331eb32aed6a5657e909
Hank Zhao <100648516+Choudoufuhezi@users.noreply.github.com>
2025-05-23 16:29:32 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 60 
-    frontend_url: str = "https://dev--digitalfamilyvault.netlify.app"
+    frontend_url: str = "https://digitalfamilyvault.netlify.app"
     backend_url: str = "https://two800-202510-dtc12-0d55.onrender.com"
 
     # Google OAuth
