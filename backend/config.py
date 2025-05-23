5094c85f3f898c8c301450e6e0204690b83b5bc5
Hank Zhao <100648516+Choudoufuhezi@users.noreply.github.com>
2025-05-22 17:59:25 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int = 60 
     frontend_url: str = "http://localhost:3000"  
     #frontend_url: str = "https://dev--digitalfamilyvault.netlify.app"
-    backend_url: str = "https://two800-202510-dtc12-pr-116.onrender.com"
+    backend_url: str = "https://two800-202510-dtc12-0d55.onrender.com"
     # backend_url: str = "http://localhost:8000"
 
     # Google OAuth
